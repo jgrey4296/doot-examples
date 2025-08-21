ebbf130cead45d0c9c8fa89327cd7004a672b7f9
John Grey <johngrey4296@gmail.com>
2025-08-21 22:54:46 +0100

    [add]: random seed examples

---
+++ 15.random.seed.jobs/local.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+
+
+"""
+# ruff: noqa:
+
+# Imports:
+
+
+# ##-- stdlib imports
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import collections
+import contextlib
+import hashlib
+from copy import deepcopy
+from uuid import UUID, uuid1
+from weakref import ref
+import atexit # for @atexit.register
+import faulthandler
+# ##-- end stdlib imports
+
+import doot
+from doot.util.dkey import DKeyed
+
+# ##-- types
+# isort: off
+# General
+import abc
+import collections.abc
+import typing
+import types
+from typing import cast, assert_type, assert_never
+from typing import Generic, NewType, Never
+from typing import no_type_check, final, override, overload
+# Protocols and Interfaces:
+from typing import Protocol, runtime_checkable
+# isort: on
+# ##-- end types
+
+# ##-- type checking
+# isort: off
+if typing.TYPE_CHECKING:
+    from typing import Final, ClassVar, Any, Self
+    from typing import Literal, LiteralString
+    from typing import TypeGuard
+    from collections.abc import Iterable, Iterator, Callable, Generator
+    from collections.abc import Sequence, Mapping, MutableMapping, Hashable
+
+    from jgdv import Maybe
+## isort: on
+# ##-- end type checking
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+# Vars:
+
+# Body:
+
+@DKeyed.formats("prefix")
+@DKeyed.types("__rng")
+def report_rng(spec, state, _prefix, _rng) -> None:
+    doot.report.wf.act(info=_prefix, msg=f"RNG id={id(_rng)}", level=logmod.WARNING)
